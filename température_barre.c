bf2f37af40a9d528385ca5f0acbdde00e777ed43
maximePRZ <67798081+maximePRZ@users.noreply.github.com>
2026-02-25 08:19:01 +0100

    Update température_barre.c

--- température_barre.c
+++ température_barre.c
@@ -6,14 +6,14 @@
 
 void main()
 {
-int longueur, temp_init, temp_bord;
+int longueur;
+float temp_init, temp_bord;
 longueur = 100;
-temp_init = 20;
-temp_bord = 100;
+temp_init = 20.0;
+temp_bord = 100.0;
 }
 
-void sim_1D(longueur, temp_bord)
+void sim_1D(int longueur, float temp_bord)
 {
 
 }
-
